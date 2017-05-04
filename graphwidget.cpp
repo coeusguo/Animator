2c4acd350491c99e9216a561542eb5b456857475
spark638 <spark638@gatech.edu>
2017-05-04 16:23:45 +0800

    bspline

--- graphwidget.cpp
+++ graphwidget.cpp
@@ -26,6 +26,7 @@
 
 #include "LinearCurveEvaluator.h"
 #include "beziercurveevaluator.h"
+#include "bsplinecurveevaluator.h"
 #include "catnullromcurveevaluator.h"
 #include "c2interpolatingcurveevaluator.h"
 
@@ -118,7 +119,7 @@ m_flcCurrCurve(FL_BLACK)
 	m_ppceCurveEvaluators[CURVE_TYPE_LINEAR] = new LinearCurveEvaluator();
 
 	// TODO: replace the linear evaluator for one of the three types of curves
-	m_ppceCurveEvaluators[CURVE_TYPE_BSPLINE] = new LinearCurveEvaluator();
+	m_ppceCurveEvaluators[CURVE_TYPE_BSPLINE] = new BSplineCurveEvaluator();
 	m_ppceCurveEvaluators[CURVE_TYPE_BEZIER] = new BezierCurveEvaluator();
 	m_ppceCurveEvaluators[CURVE_TYPE_CATMULLROM] = new CatnullRomEvaluator();
 	// Note that C2-Interpolating curve is not a requirement
